ea2f7cc046d98548900e5c7a853acb831935b0dc
piercefrawley <pierce@procore.com>
2015-11-06 21:43:46 -0800

    Client Server Folders

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var React = require('react')
+var Router = require('react-router')
+
+var routes = require('./routes')
+
+var app = express()
+
+// ...express config...
+
+app.use(function(req, res, next) {
+  var router = Router.create({location: req.url, routes: routes})
+  router.run(function(Handler, state) {
+    var html = React.renderToString(<Handler/>)
+    return res.render('react_page', {html: html})
+  })
+})
