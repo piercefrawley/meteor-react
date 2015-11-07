6563172dab6d2302e441691cbd513a7c28a70e7c
piercefrawley <pierce@procore.com>
2015-11-06 22:23:50 -0800

    Everything works properly again

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-var express = require('express')
-var React = require('react')
-var Router = require('react-router')
+// var express = require('express')
+// var React = require('react')
+// var Router = require('react-router')
 
-var routes = require('./routes')
+// var routes = require('./routes')
 
-var app = express()
+// var app = express()
 
-// ...express config...
+// // ...express config...
 
-app.use(function(req, res, next) {
-  var router = Router.create({location: req.url, routes: routes})
-  router.run(function(Handler, state) {
-    var html = React.renderToString(<Handler/>)
-    return res.render('react_page', {html: html})
-  })
-})
+// app.use(function(req, res, next) {
+//   var router = Router.create({location: req.url, routes: routes})
+//   router.run(function(Handler, state) {
+//     var html = React.renderToString(<Handler/>)
+//     return res.render('react_page', {html: html})
+//   })
+// })
