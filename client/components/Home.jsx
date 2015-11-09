16ac8b8c4f696e6db86e7ea6335200a77d4cb20f
piercefrawley <pierce@procore.com>
2015-11-08 22:34:05 -0800

    Still Need to Fix Routes

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,15 +9,18 @@ Home = React.createClass({
         <Navbar inverse toggleNavKey={0}>
           <Nav right eventKey={0}>
             <NavItem eventKey={1} href="#">
-              <Link to="/">Home</Link>
+              <div onClick={e => e.stopPropagation()}>
+                <Link to="/">Home</Link>
+              </div>
             </NavItem>
             <NavItem divider/>
             <NavItem eventKey={2}>
-              <Link to="/login">Login</Link>
+              <div onClick={e => {debugger; e.stopPropagation()}}>  
+                <Link to="/login">Login</Link>
+              </div>
             </NavItem>
           </Nav>
         </Navbar>
-        <iframe src="https://media.giphy.com/media/5ehBR5qtLEXBe/giphy.gif" width="480" height="270 " frameBorder="0" allowFullScreen/>
         <p>This is the home route.</p>
       </div>
     );
