c79cf58e672884b354bf226e1cfdc2073b7133c4
piercefrawley <pierce@procore.com>
2015-11-15 15:31:22 -0800

    Shameless Green

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,26 +1,11 @@
 const { Link } = ReactRouter;
-const { Nav, Navbar, NavItem } = ReactBootstrap
 
 // App component - represents the whole app
 Home = React.createClass({
   render() {
     return (
       <div>
-        <Navbar inverse toggleNavKey={0}>
-          <Nav right eventKey={0}>
-            <NavItem eventKey={1} href="#">
-              <div onClick={e => e.stopPropagation()}>
-                <Link to="/">Home</Link>
-              </div>
-            </NavItem>
-            <NavItem divider/>
-            <NavItem eventKey={2}>
-              <div onClick={e => {debugger; e.stopPropagation()}}>  
-                <Link to="/login">Login</Link>
-              </div>
-            </NavItem>
-          </Nav>
-        </Navbar>
+        <Link to='/login'>Login Page</Link>
         <p>This is the home route.</p>
       </div>
     );
